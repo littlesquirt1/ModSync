ec9602ca8be9d70533c91a33b2159b3a8cfd66cf
littlesquirt1 <codyc9999@gmail.com>
2025-02-19 01:37:39 -0500

    Make the mod not crash the game

--- src/main/java/com/cody/modsync/ModSync.java
+++ src/main/java/com/cody/modsync/ModSync.java
@@ -24,81 +24,83 @@ import java.util.Set;
 public class ModSync
 {
     private static final Logger LOGGER = LogUtils.getLogger();
-    public ModSync() throws MalformedURLException {
+    public ModSync() {
         // Client only
         if (FMLEnvironment.dist != Dist.CLIENT) {
             return;
         }
+        try {
 //======================================================================================================================
-        // Get update url from config file
-        File configFile = new File(System.getProperty("user.dir"), "config" + File.separator + "modsync.txt");
+            // Get update url from config file
+            File configFile = new File(System.getProperty("user.dir"), "config" + File.separator + "modsync.txt");
 
-        // If there is a config, check for updates. Otherwise, just launch the GUI for initial setup.
-        // If there are no updates, return.
-        if (configFile.exists()) {
-            URL updateUrl = null;
-            try {
-                updateUrl = toUrl(Files.readString(configFile.toPath()).trim());
-            } catch (IOException e) {
-                // TODO: Add message on game startup if the config file is invalid. Don't crash game.
-                LOGGER.error("Failed to read config file.", e);
-                throw new RuntimeException("Failed to read config file.", e);
-            }
-//======================================================================================================================
-            // Get server mod array
-            Set<String> serverModList = new HashSet<>(Arrays.asList(getFromUrl(new URL(updateUrl, "modlist")).split("/")));
-            serverModList.add(getFromUrl(new URL(updateUrl, "mod_sync_jar_name")));
-
-
-            // Get local mod file name array
-            File modsDir = new File(System.getProperty("user.dir"), "mods");
-            // If modsDir is valid. Otherwise, let the GUI handle it.
-            if (modsDir.exists() && modsDir.isDirectory()) {
-                // If mods match server, return. Otherwise, run the updater GUI.
-                String[] localModList = modsDir.list();
-                if (localModList != null && serverModList.equals(new HashSet<>(Arrays.asList(localModList)))) {
-                    return;
+            // If there is a config, check for updates. Otherwise, just launch the GUI for initial setup.
+            // If there are no updates, return.
+            if (configFile.exists()) {
+                URL updateUrl;
+                try {
+                    updateUrl = toUrl(Files.readString(configFile.toPath()).trim());
+                } catch (IOException e) {
+                    // TODO: Add message on game startup if the config file is invalid. Don't crash game.
+                    LOGGER.error("Failed to read config file.", e);
+                    throw new RuntimeException("Failed to read config file.", e);
                 }
-            }
+                //======================================================================================================================
+                // Get server mod array
+                Set<String> serverModList = new HashSet<>(Arrays.asList(getFromUrl(new URL(updateUrl, "modlist")).split("/")));
+                serverModList.add(getFromUrl(new URL(updateUrl, "mod_sync_jar_name")));
 
-        }
-//======================================================================================================================
-        // Get the running jar file
-        File jarFile = new File(ModList
-                .get()
-                .getModContainerById("modsync")
-                .orElseThrow()
-                .getModInfo()
-                .getOwningFile()
-                .getFile()
-                .getFilePath()
-                .toString()
-        );
-//======================================================================================================================
-        // Launch the GUI
 
-        if (jarFile.exists()) {
-            try {
-                // Get java executable path
-                String javaPath = System.getProperty("java.home") + File.separator + "bin" + File.separator + "java";
-                if (System.getProperty("os.name").toLowerCase().contains("win")) {
-                    javaPath += ".exe";
+                // Get local mod file name array
+                File modsDir = new File(System.getProperty("user.dir"), "mods");
+                // If modsDir is valid. Otherwise, let the GUI handle it.
+                if (modsDir.exists() && modsDir.isDirectory()) {
+                    // If mods match server, return. Otherwise, run the updater GUI.
+                    String[] localModList = modsDir.list();
+                    if (localModList != null && serverModList.equals(new HashSet<>(Arrays.asList(localModList)))) {
+                        return;
+                    }
                 }
-                // Run the external JAR
-                Runtime.getRuntime().exec(new String[]{javaPath, "-jar", jarFile.getAbsolutePath()});
 
-                // Gracefully stop the game
-                System.exit(0);
-            } catch (IOException e) {
-                // TODO: Add message on game startup if the GUI fails to launch. Don't crash game.
-                LOGGER.error("Failed to launch external GUI.", e);
-                throw new RuntimeException("Failed to launch external GUI.", e);
             }
-        } else {
-            // TODO: Add message on game startup if the JAR file is missing. Don't crash game.
-            LOGGER.error("JAR file not found: " + jarFile.getAbsolutePath());
-            throw new RuntimeException("JAR file not found: " + jarFile.getAbsolutePath());
-        }
+            //======================================================================================================================
+            // Get the running jar file
+            File jarFile = new File(ModList
+                    .get()
+                    .getModContainerById("modsync")
+                    .orElseThrow()
+                    .getModInfo()
+                    .getOwningFile()
+                    .getFile()
+                    .getFilePath()
+                    .toString()
+            );
+            //======================================================================================================================
+            // Launch the GUI
+
+            if (jarFile.exists()) {
+                try {
+                    // Get java executable path
+                    String javaPath = System.getProperty("java.home") + File.separator + "bin" + File.separator + "java";
+                    if (System.getProperty("os.name").toLowerCase().contains("win")) {
+                        javaPath += ".exe";
+                    }
+                    // Run the external JAR
+                    Runtime.getRuntime().exec(new String[]{javaPath, "-jar", jarFile.getAbsolutePath()});
+
+                    // Gracefully stop the game
+                    System.exit(0);
+                } catch (IOException e) {
+                    // TODO: Add message on game startup if the GUI fails to launch. Don't crash game.
+                    LOGGER.error("Failed to launch external GUI.", e);
+                    throw new RuntimeException("Failed to launch external GUI.", e);
+                }
+            } else {
+                // TODO: Add message on game startup if the JAR file is missing. Don't crash game.
+                LOGGER.error("JAR file not found: " + jarFile.getAbsolutePath());
+                throw new RuntimeException("JAR file not found: " + jarFile.getAbsolutePath());
+            }
+        } catch (Exception ignored) {}
     }
 
     private static URL toUrl(String urlString) {
